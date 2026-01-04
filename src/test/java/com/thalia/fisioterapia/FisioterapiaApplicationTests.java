7c07add50694d7eba542b52c659019907239c8e2
Estevao Fidelis <rat871@gmail.com>
2026-01-04 17:14:55 -0300

    Projeto inicial

---
+++ src/test/java/com/thalia/fisioterapia/FisioterapiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalia.fisioterapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisioterapiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
